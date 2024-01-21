10af2805861b5a0e4a1cfdb6f39eaf696bde3358
sowmiya <sowmiyar.21ece@kongu.edu>
2024-01-21 20:48:41 +0530

    javascript operators file

---
+++ javascirpt/operators.js
@@ -0,0 +1,29 @@
+/*
+//assignment operator
+var a=10;
+console.log("assignment operator output:",a)
+//arthimetic operator:+,-,*,/,%
+var aA=25
+var bA=10
+console.log(typeof(aA))
+console.log("Arithmtic operator  ADDITION output:",aA+bA)
+console.log("Arithmtic operator  SUBTRACTION output:",aA-bA)
+console.log("Arithmtic operator  MULTIPLICATION output:",aA*bA)
+console.log("Arithmtic operator  DIVISION output:",aA/bA)
+console.log("Arithmtic operator  MODULUS output:",aA%bA)
+//Logical operator -> &&,||,~
+var aL=10
+var bL=20
+console.log("LOGICAL AND operator output:",aL&&bL)
+console.log("LOGICAL OR operator output:",aL||bL)
+console.log("LOGICAL NOT operator output:",!bL)
+console.log(10 && 30)
+console.log(10 && 50)
+console.log(0 && 10)
+console.log(100 && 0)*/
+console.log(10 || 20)
+console.log(10 || 0)
+console.log(0 || 10)
+console.log(10 || 20 && 0)
+console.log((100 ||20)&& 0)
+console.log(0 && (10||20))
