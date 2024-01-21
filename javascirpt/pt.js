257f83dccdb0310d70e31d3dc95ef65820d117ba
sowmiya <sowmiyar.21ece@kongu.edu>
2024-01-21 20:51:08 +0530

    variables file

---
+++ javascirpt/pt.js
@@ -0,0 +1,14 @@
+a='a'
+console.log("general variable declaration a='a:",a)
+var b = 'var'
+console.log("this variable belongs to var b='var' type",b)
+b=10
+console.log("this variable belongs to var type",b)
+
+let c='let'
+console.log("this variable belongs to let c ='let' type",c)
+const d = 100
+console.log("this variable belongs to const type",d)
+d=200
+console.log("this variable belongs to changed const type",d0)
+
